fafee0609c5642dbeb4c58379063f4b6975cd4ae
Maciej Bryński <maciek@brynski.pl>
2020-07-08 15:58:16 +0200

    Change topic name

--- spark/aggregation.py
+++ spark/aggregation.py
@@ -10,7 +10,7 @@ df = spark \
   .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", "bmi2") \
+  .option("subscribe", "bmi") \
   .load()
 
 
