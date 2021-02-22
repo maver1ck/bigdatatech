f8b24a8b3915cb6dd4ef8eb765b6b04f8a9dff16
Maciej Bryński <maciek@brynski.pl>
2021-02-22 14:19:21 +0100

    Add functions import

--- spark/aggregation.py
+++ spark/aggregation.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, DoubleType
+from pyspark.sql.functions import *
 
 # initializing Spark
 spark = SparkSession.builder.appName("BMI Aggregator").getOrCreate()
@@ -7,21 +8,22 @@ spark.sparkContext.setLogLevel('WARN')
 
 # read from Kafka
 df = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", "bmi") \
-  .load()
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "bmi") \
+    .load()
 
 
 # extract data from json
 # PUT your column name here
-values = df.selectExpr('from_json(CAST(value AS STRING), "__column_name__ DOUBLE").__column__name__ as __column_name__')
+values = df.selectExpr(
+    'from_json(CAST(value AS STRING), "__column_name__ DOUBLE").__column__name__ as __column_name__')
 
 # TODO: Aggregation needs to be added here
 
 # This will be needed when running aggregation
-# .outputMode("complete") 
+# .outputMode("complete")
 
 query = values \
     .writeStream \
